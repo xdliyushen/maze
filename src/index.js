4726b4e5c4edcea1f3f7a98c9295f31783710bfd
mettli <37898499+xdliyushen@users.noreply.github.com>
2022-08-17 00:19:01 +0800

    feat: 时间旅行

--- src/index.js
+++ src/index.js
@@ -3,27 +3,214 @@ import random from "lodash/random";
 import intersection from 'lodash/intersection';
 import xor from 'lodash/xor';
 import shuffle from 'lodash/shuffle';
+import cloneDeep from 'lodash/cloneDeep';
 import './styles.css';
 // todo 构建表单
 // todo 时间旅行
 // todo 动画速率
 
+const getRandomUnvisitedCell = (cells = [], exceptions = []) => {
+    const unvisitedCells = xor(cells, exceptions);
+
+    if (!unvisitedCells.length) {
+        return null;
+    }
+
+    const cellIndex = random(0, unvisitedCells.length - 1);
+    const randomCell = unvisitedCells[cellIndex];
+
+    return randomCell;
+}
+
 class Grid {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+
+        this.currentStep = 0;
+
         this.cells = [];
 
-        for (let i = 0; i < height; i++) {
+        this.steps = [];
+
+        this._createCells();
+        this._generateSteps();
+    }
+
+    _createCells() {
+        for (let i = 0; i < this.height; i++) {
             const row = [];
 
-            for (let j = 0; j < width; j++) {
+            for (let j = 0; j < this.width; j++) {
                 const cell = new Cell(j, i, this);
                 row.push(cell);
             }
 
             this.cells.push(row);
         }
+    }
+
+    _generateSteps() {
+        const cells = flattenDeep(this.cells);
+
+        const maze = [getRandomUnvisitedCell(cells)];
+
+        while (maze.length < cells.length) {
+            let path = [getRandomUnvisitedCell(cells, maze)];
+            // 二维数组 存储 path 中已经做出的选择以及不能用的选择
+            let currCell = path[0];
+            let blackList = [];
+
+            this.steps.push(
+                this._snapshot(maze.concat(path), currCell),
+            );
+
+            while (1) {
+                const deadEndCells = blackList[blackList.length - 1] || [];
+                const currCellNeighbors = shuffle(currCell.getNeighbors()
+                    // 不能往回走
+                    .filter(v => path.indexOf(v) === -1 && deadEndCells.indexOf(v) === -1));
+                // 是否与迷宫联通
+                const isPathComplete = !!(intersection(currCellNeighbors, maze).length);
+
+                if (isPathComplete) {
+                    const inters = intersection(currCellNeighbors, maze);
+                    const neighbor = inters[random(0, inters.length - 1)];
+
+                    const direction = this._getDirection(currCell, neighbor);
+                    currCell.connections[direction] = true;
+                    const direction2 = this._getDirection(neighbor, currCell);
+                    neighbor.connections[direction2] = true;
+
+                    maze.push(...path);
+                    path = [getRandomUnvisitedCell(cells, maze)];
+                    currCell = path[0];
+                    blackList = [];
+                    break;
+                } else {
+                    let isAllLoop = true;
+                    for (const nextCell of currCellNeighbors) {
+                        const nextCellNeighbors = nextCell.getNeighbors();
+
+                        // 是否在当前路径形成环形
+                        const isLoop = !(intersection(nextCellNeighbors, path).length);
+
+                        if (isLoop) {
+                            continue;
+                        } else {
+                            const direction = this._getDirection(currCell, nextCell);
+                            currCell.connections[direction] = true;
+                            const direction2 = this._getDirection(nextCell, currCell);
+                            nextCell.connections[direction2] = true;
+
+                            isAllLoop = false;
+                            path.push(nextCell);
+                            blackList.push([nextCell]);
+                            currCell = nextCell;
+                            break;
+                        }
+                    }
+
+                    if (isAllLoop) {
+                        if (path.length === 1) {
+                            const randomCell = getRandomUnvisitedCell(cells, maze);
+                            path = [randomCell];
+                            blackList = [[]];
+                            currCell = randomCell;
+                        } else {
+                            path.pop();
+                            const lastPath = path[path.length - 1];
+
+                            const direction = this._getDirection(lastPath, currCell);
+                            lastPath.connections[direction] = false;
+                            const direction2 = this._getDirection(currCell, lastPath);
+                            currCell.connections[direction2] = false;
+
+                            blackList.pop();
+                            blackList[blackList.length - 1].push(currCell);
+                            currCell = path[path.length - 1];
+                        }
+                    }
+
+                    this.steps.push(
+                        this._snapshot(maze.concat(path), currCell),
+                    );
+                }
+            }
+        }
+    }
+
+    _getDirection(cell, neighbor) {
+        if (cell.position.x === neighbor.position.x) {
+            return cell.position.y > neighbor.position.y ? 'top' : 'bottom';
+        }
+
+        return cell.position.x > neighbor.position.x ? 'left' : 'right';
+    }
+
+    _snapshot(maze, currCell) {
+        const cellsClone = cloneDeep(this.cells.map(row => {
+            return row.map(v => {
+                return {
+                    position: v.position,
+                    connections: v.connections,
+                };
+            });
+        }));
+        const mazeClone = maze.map(cell => {
+            const { x, y } = cell.position;
+            return cellsClone[y][x]
+        });
+        const { x: currCellX, y: currCellY, } = currCell.position;
+        return {
+            gridCells: cellsClone,
+            maze: mazeClone,
+            currCell: cellsClone[currCellY][currCellX],
+        }
+    }
+
+    paintCurrentStep() {
+        const context = this.steps[this.currentStep];
+
+        if (!context) {
+            return;
+        }
+
+        const { gridCells, maze, currCell } = context;
+        const app = document.querySelector('#app');
+        app.innerHTML = '';
+
+        for (const row of gridCells) {
+            const rowEl = document.createElement('div');
+            rowEl.className = 'row';
+
+            for (const cell of row) {
+                const cellEl = document.createElement('div');
+                cellEl.className = 'cell';
+                cellEl.style.borderColor = `${cell.connections.top ? 'transparent' : '#fff'} ${cell.connections.right ? 'transparent' : '#fff'} ${cell.connections.bottom ? 'transparent' : '#fff'} ${cell.connections.left ? 'transparent' : '#fff'}`;
+
+                if (maze.indexOf(cell) > -1) {
+                    cellEl.style.background = 'transparent';
+                    cellEl.style.borderColor = `${cell.connections.top ? 'transparent' : '#000'} ${cell.connections.right ? 'transparent' : '#000'} ${cell.connections.bottom ? 'transparent' : '#000'} ${cell.connections.left ? 'transparent' : '#000'}`;
+                }
+
+                if (cell === currCell) {
+                    cellEl.style.background = 'red';
+                }
+
+                rowEl.appendChild(cellEl);
+            }
+
+            app.appendChild(rowEl);
+        }
+    }
+
+    prevStep() {
+        this.currentStep = Math.max(0, this.currentStep - 1);
+    }
+
+    nextStep() {
+        this.currentStep = Math.min(this.steps.length - 1, this.currentStep + 1);
     }
 
     getCell(x, y) {
@@ -76,147 +263,20 @@ class Cell {
     }
 }
 
-// todo 修改
 // const WIDTH = 15;
 // const HEIGHT = 15;
 const WIDTH = 5;
 const HEIGHT = 5;
 
-// 绘制
-const paint = (grid, maze, currCell) => {
-    const app = document.querySelector('#app');
-    app.innerHTML = '';
-
-    for (const row of grid.cells) {
-        const rowEl = document.createElement('div');
-        rowEl.className = 'row';
-
-        for (const cell of row) {
-            const cellEl = document.createElement('div');
-            cellEl.className = 'cell';
-            cellEl.style.borderColor = `${cell.connections.top ? 'transparent' : '#fff'} ${cell.connections.right ? 'transparent' : '#fff'} ${cell.connections.bottom ? 'transparent' : '#fff'} ${cell.connections.left ? 'transparent' : '#fff'}`; 
-
-            if(maze.indexOf(cell) >= 0) {
-                cellEl.style.background = 'transparent';
-                cellEl.style.borderColor = `${cell.connections.top ? 'transparent' : '#000'} ${cell.connections.right ? 'transparent' : '#000'} ${cell.connections.bottom ? 'transparent' : '#000'} ${cell.connections.left ? 'transparent' : '#000'}`; 
-            }
-
-            if(cell === currCell) {
-                cellEl.style.background = 'red';
-            }
-
-            rowEl.appendChild(cellEl);
-        }
-
-        app.appendChild(rowEl);
-    }
-}
-
-const getRandomUnvisitedCell = (cells = [], exceptions = []) => {
-    const unvisitedCells = xor(cells, exceptions);
-
-    if (!unvisitedCells.length) {
-        return null;
-    }
-
-    const cellIndex = random(0, unvisitedCells.length - 1);
-    const randomCell = unvisitedCells[cellIndex];
-
-    return randomCell;
-}
-
-const getDirection = (cell, neighbor) => {
-    if (cell.position.x === neighbor.position.x) {
-        return cell.position.y > neighbor.position.y ? 'top' : 'bottom';
-    }
-
-    return cell.position.x > neighbor.position.x ? 'left' : 'right';
-}
-
 const grid = new Grid(WIDTH, HEIGHT);
-const cells = flattenDeep(grid.cells);
 
-const maze = [getRandomUnvisitedCell(cells)];
-let path = [getRandomUnvisitedCell(cells, maze)];
-// 二维数组 存储 path 中已经做出的选择以及不能用的选择
-let currCell = path[0];
-let blackList = [];
+document.querySelector('#prev-step-button').addEventListener('click', () => {
+    grid.prevStep();
+    grid.paintCurrentStep();
+});
+document.querySelector('#next-step-button').addEventListener('click', () => {
+    grid.nextStep();
+    grid.paintCurrentStep();
+});
 
-const nextStep = () => {
-    const deadEndCells = blackList[blackList.length - 1] || [];
-    const currCellNeighbors = shuffle(currCell.getNeighbors()
-        // 不能往回走
-        .filter(v => path.indexOf(v) === -1 && deadEndCells.indexOf(v) === -1));
-    // 是否与迷宫联通
-    const isPathComplete = !!(intersection(currCellNeighbors, maze).length);
-
-    if (isPathComplete) {
-        const inters = intersection(currCellNeighbors, maze);
-        const neighbor = inters[random(0, inters.length - 1)];
-
-        const direction = getDirection(currCell, neighbor);
-        currCell.connections[direction] = true;
-        const direction2 = getDirection(neighbor, currCell);
-        neighbor.connections[direction2] = true;
-
-        maze.push(...path);
-        path = [getRandomUnvisitedCell(cells, maze)];
-        currCell = path[0];
-        blackList = [];
-    } else {
-        let isAllLoop = true;
-        for (const nextCell of currCellNeighbors) {
-            const nextCellNeighbors = nextCell.getNeighbors();
-
-            // 是否在当前路径形成环形
-            const isLoop = !(intersection(nextCellNeighbors, path).length);
-
-            if (isLoop) {
-                continue;
-            } else {
-                const direction = getDirection(currCell, nextCell);
-                currCell.connections[direction] = true;
-                const direction2 = getDirection(nextCell, currCell);
-                nextCell.connections[direction2] = true;
-
-                isAllLoop = false;
-                path.push(nextCell);
-                blackList.push([nextCell]);
-                currCell = nextCell;
-                break;
-            }
-        }
-
-        if (isAllLoop) {
-            if (path.length === 1) {
-                const randomCell = getRandomUnvisitedCell(cells, maze);
-                path = [randomCell];
-                blackList = [[]];
-                currCell = randomCell;
-            } else {
-                path.pop();
-                const lastPath = path[path.length - 1];
-
-                const direction = getDirection(lastPath, currCell);
-                lastPath.connections[direction] = false;
-                const direction2 = getDirection(currCell, lastPath);
-                currCell.connections[direction2] = false;
-
-                blackList.pop();
-                blackList[blackList.length - 1].push(currCell);
-                currCell = path[path.length - 1];
-            }
-        }
-    }
-
-    paint(grid, maze.concat(path), currCell);
-};
-
-const prevStep = () => {
-    // todo
-};
-
-document.querySelector('#prev-step-button').addEventListener('click', prevStep)
-document.querySelector('#next-step-button').addEventListener('click', nextStep)
-
-paint(grid, maze.concat(path), currCell);
+grid.paintCurrentStep();
